159ae18e312b406e7e728227978c6e41f6ae5bfe
Wesolowski, Robert <robert.wesolowski@gft.com>
2016-06-17 11:50:55 +0200

    Use gpio instead of pi-gpio

--- backend/router.js
+++ backend/router.js
@@ -1,39 +1,43 @@
 /*global require, module, console*/
 var express = require('express'),
     status = require('http-status'),
-    gpio = require('pi-gpio'),
-    router = express.Router();
+    gpio = require('gpio'),
+    router = express.Router(),
+    gpioArray = [],
+    index;
+
+for (index = 17; index < 18; index += 1) {
+    gpioArray[index] = gpio.export(index, {
+        direction: 'out',
+        interval: 200,
+        ready: function () {
+            console.log('Pin ' + index + ' was exported');
+        }
+    });
+}
 
 router.get('/enable/:id', function (request, response) {
     var id = request.params.id;
-    console.log('Enabling ' + id);
-    gpio.open(id, 'output', function (error) {
-        if (error) {
-            console.log(error);
-            return;
-        }
-        gpio.write(id, 1, function () {
-            gpio.close(id);
-        });
-    });
-    response.status(status.OK)
-        .send();
+    if (gpioArray[id] === undefined) {
+        response.status(status.NOT_FOUND)
+            .send();
+    } else {
+        gpioArray[id].set();
+        response.status(status.OK)
+            .send();
+    }
 });
 
 router.get('/disable/:id', function (request, response) {
     var id = request.params.id;
-    console.log('Disabling ' + id);
-    gpio.open(id, 'output', function (error) {
-        if (error) {
-            console.log(error);
-            return;
-        }
-        gpio.write(id, 0, function () {
-            gpio.close(id);
-        });
-    });
-    response.status(status.OK)
-        .send();
+    if (gpioArray[id] === undefined) {
+        response.status(status.NOT_FOUND)
+            .send();
+    } else {
+        gpioArray[id].reset();
+        response.status(status.OK)
+            .send();
+    }
 });
 
 module.exports = router;
